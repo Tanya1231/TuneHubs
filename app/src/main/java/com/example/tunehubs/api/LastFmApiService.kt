f7951b2be26f6a7ee6377d95bac1dc8d6816a89f
tanjaabdulova@yandex.ru <tanjaabdulova@yandex.ru>
2025-08-05 12:32:13 +0500

    edit code

---
+++ app/src/main/java/com/example/tunehubs/api/LastFmApiService.kt
@@ -0,0 +1,18 @@
+package com.example.tunehubtest.api
+
+import com.example.tunehubs.models.TrackInfoResponse
+import com.example.tunehubtest.models.TopTracksResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LastFmApiService {
+    @GET("2.0/?method=chart.gettoptracks&api_key=962cc9583cc6238050031519fd21a7ae&format=json")
+    suspend fun getTopTracks(@Query("limit") limit: Int = 10): Response<TopTracksResponse>
+
+    @GET("2.0/?method=track.getinfo&api_key=962cc9583cc6238050031519fd21a7ae&format=json")
+    suspend fun getTrackInfo(
+        @Query("artist") artist: String,
+        @Query("track") track: String
+    ): Response<TrackInfoResponse>
+}
---
+++ app/src/main/java/com/example/tunehubs/api/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.example.tunehubs.api
+
+import com.example.tunehubtest.api.LastFmApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://ws.audioscrobbler.com/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    val apiService: LastFmApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(LastFmApiService::class.java)
+    }
+}
